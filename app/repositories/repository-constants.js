8b10570332fa0daf704026f6beb14e41f1399d5b
sammosampson <sammosampson@gmail.com>
2015-07-05 09:01:42 +0100

    Removed repositories

--- app/repositories/repository-constants.js
+++
@@ -1,3 +0,0 @@
-export default {
-	FETCHING: 'FETCHING'
-}
--- app/repositories/repository-slave.js
+++
@@ -1,6 +0,0 @@
-export default {
-    fetch: (username) => {
-      let url = `https://api.github.com/users/${username}/repos`;
-      return fetch(url).then(response => response.json());
-  }
-}
