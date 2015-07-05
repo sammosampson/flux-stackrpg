8bc6804a102d3c4151f6d07403ba3dc2ab5950a6
sammosampson <sammosampson@gmail.com>
2015-07-05 18:18:36 +0100

    Added gold counter

---
+++ app/loot/gold-counter.react.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class GoldCounter extends React.Component {
+  render() {
+    return (
+      <div>
+        <label>Gold:</label>
+        <div>10</div>
+      </div>
+    );
+  }
+}
--- app/stack-rpg-frame.react.js
+++ app/stack-rpg-frame.react.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Timer from './timer/timer.react';
+import GoldCounter from './loot/gold-counter.react';
 
 export default class StackRpgFrame extends React.Component {
   render() {
     return (
       <div>
         <Timer />
+        <GoldCounter />
       </div>
     );
   }
