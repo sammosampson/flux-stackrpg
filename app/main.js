3e4fac3a3567838e4bfea50f701a6c0c1c97257c
sammosampson <sammosampson@gmail.com>
2015-07-06 23:02:17 +0100

    Got rid of render warning

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import StackRpgFrame from './stack-rpg-frame.react';
 
-React.render(<StackRpgFrame />, document.body);
+React.render(<StackRpgFrame />, document.getElementById('app'));
