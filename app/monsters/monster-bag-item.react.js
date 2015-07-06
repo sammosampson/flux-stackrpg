7299b8384f86af722e1864c5a6e62366652fb02d
sammosampson <sammosampson@gmail.com>
2015-07-06 07:07:38 +0100

    Fixed up key issues

--- app/monsters/monster-bag-item.react.js
+++ app/monsters/monster-bag-item.react.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 export default class MonsterBagItem extends React.Component {
+  _selectMonster() {
+    console.log(this.props.monsterName);
+  }
   render() {
     return(
-      <div key={this.props.monsterName} className="bagItem">
-        <div className="noselect">{this.props.monsterName}</div>
+      <div key={this.props.key}>
+        <div onClick={this._selectMonster.bind(this)}>{this.props.monsterName}</div>
       </div>
       );
   }
--- app/monsters/monster-bag-list.react.js
+++ app/monsters/monster-bag-list.react.js
@@ -4,9 +4,9 @@ import MonsterBagItem from './monster-bag-item.react';
 export default class MonsterBagList extends React.Component {
   render() {
     return (
-      <div className="clearfix"> {
-        this.props.monsters.map((monster) => {
-          return <MonsterBagItem monsterName={monster.Name}/>;
+      <div> {
+        this.props.monsters.map((monster, index) => {
+          return <MonsterBagItem monsterName={monster.Name} key={index}/>;
         })}
       </div>);
   }
