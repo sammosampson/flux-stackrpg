38cab930785fe2ae580dd4164d4e3b09750aed75
sammosampson <sammosampson@gmail.com>
2015-07-06 08:56:23 +0100

    Added timer change kill monster hookup

--- app/battles/battle-stack-store.js
+++ app/battles/battle-stack-store.js
@@ -3,13 +3,25 @@
 import AppDispatcher from '../dispatcher';
 import MonsterBagConstants from '../monsters/monster-bag-constants';
 import Monster from './monster';
+import TimerStore from '../timer/timer-store';
 import EventEmitter from 'events';
 
 let state = {
-	stack : []
+	stack : [],
+	deadMonsters : []
 };
 
 class BattleStackStore extends EventEmitter.EventEmitter {
+	constructor()	{
+		super();
+		TimerStore.addChangeListener(this._killMonster.bind(this));
+	}
+
+	_killMonster() {
+    state.deadMonsters.push(state.stack.shift);
+		this.emitChange();
+  }
+
 	_add(monsterName) {
 		state.stack.push(new Monster(monsterName));
 		this.emitChange();
